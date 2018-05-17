359063507fdae54e7710feced046a038189ca0ef
pedronaresi <pmnaresi@hotmail.com>
2018-05-17 18:38:27 -0300

    .

--- garbage-diposal-project/garbage-diposal-project.ino
+++ garbage-diposal-project/garbage-diposal-project.ino
@@ -1,21 +1,21 @@
 #include <Servo.h>
 
 //Infra Red Sensors pin declaration
-const int infraRedPin1 = 1;
-const int infraRedPin2 = 2;
-const int infraRedPin3 = 3;
+const int infraRedPin1 = 24;
+const int infraRedPin2 = 21;
+const int infraRedPin3 = 22;
 
 //Bargraph LEDs pin declaration
-const int ledPin1 = 4;
-const int ledPin2 = 5;
-const int ledPin3 = 6;
-const int ledPin4 = 7;
-const int ledPin5 = 8;
-const int ledPin6 = 9;
-const int ledPin7 = 10;
-const int ledPin8 = 11;
-const int ledPin9 = 12;
-const int ledPin10 = 13;
+const int ledPin1 = 5;
+const int ledPin2 = 6;
+const int ledPin3 = 7;
+const int ledPin4 = 8;
+const int ledPin5 = 9;
+const int ledPin6 = 10;
+const int ledPin7 = 11;
+const int ledPin8 = 12;
+const int ledPin9 = 4;
+const int ledPin10 = 3;
 
 //Infra Red Sensors States
 int infraRedPin1State = 0;
@@ -28,14 +28,23 @@ void setup() {
   pinMode(infraRedPin1, INPUT);
   pinMode(infraRedPin2, INPUT);
   pinMode(infraRedPin3, INPUT);
-
+  pinMode(ledPin1, OUTPUT);
+  pinMode(ledPin2, OUTPUT);
+  pinMode(ledPin3, OUTPUT);
+  pinMode(ledPin4, OUTPUT);
+  pinMode(ledPin5, OUTPUT);
+  pinMode(ledPin6, OUTPUT);
+  pinMode(ledPin7, OUTPUT);
+  pinMode(ledPin8, OUTPUT);
+  pinMode(ledPin9, OUTPUT);
+  pinMode(ledPin10, OUTPUT);
 }
 
 void loop() {
   //Garbage Level Infra Red Sensors
-  infraRedPin1State = digitalRead(infraRedPin1);
-  infraRedPin2State = digitalRead(infraRedPin2);
-  infraRedPin3State = digitalRead(infraRedPin3);
+  infraRedPin1State = !digitalRead(infraRedPin1);
+  infraRedPin2State = !digitalRead(infraRedPin2);
+  infraRedPin3State = !digitalRead(infraRedPin3);
 
   if (infraRedPin1State == 1 && infraRedPin2State == 1 && infraRedPin3State == 1){
     //Dez LEDs acessos.
